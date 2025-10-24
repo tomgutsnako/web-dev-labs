2c14d928d403a94514e915042750a7304cee6659
Tom Berg Bautista <tomberg.bautista@gmail.com>
2025-10-24 12:55:36 +0800

    updated mini cafe and lab11

--- lab11/menu-script.js
+++
@@ -1,7 +0,0 @@
-let orders = [];
-
-function order(item) {
-  alert("You ordered " + item + "!");
-  orders.push(item);
-  document.getElementById("summary").innerText = "Orders: " + orders.join(", ");
-}
